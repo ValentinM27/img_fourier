d8bcdf03e40d7894a16452716d42da7ee7c4ab5a
Valentin Marguerie <74249732+ValentinM27@users.noreply.github.com>
2022-11-30 13:10:13 +0100

    [FFT 2D && IFFT 2D]

--- fourier.py
+++ fourier.py
@@ -1,14 +1,16 @@
 # Import des librairies
 import matplotlib.pyplot as plt
 import numpy as np
+import math
 from PIL import Image as i, ImageOps
 
 # Affichage CLI
 from tqdm import tqdm
+from sys import exit
 
 def main():
 	# Traitement image
-	image = i.open("cafe.jpg")
+	image = i.open("lena.jpg")
 	gray_image = ImageOps.grayscale(image)
 	pixels = list(gray_image.getdata())
 	width, height = image.size
@@ -36,6 +38,25 @@ def main():
 		plt.subplot(133)
 		plt.title("IDFT 2D")
 		plt.imshow(image_idft_2D, cmap='gray', vmin=0, vmax=255)
+
+		plt.show()
+
+	if choice == 2:
+		# fft 2D
+		fft_2D = fast_fourier_transform_2D(pixels);
+		ifft_2D = invert_fast_fourier_transform_2D(fft_2D)
+
+		plt.subplot(131)
+		plt.title("Image originale convertie en gris")
+		plt.imshow(pixels, cmap='gray', vmin=0, vmax=255)
+
+		plt.subplot(132)
+		plt.title("FFT 2D")
+		plt.imshow(np.abs(fft_2D), cmap='gray', vmin=0, vmax=255)
+
+		plt.subplot(133)
+		plt.title("IFFT 2D")
+		plt.imshow(ifft_2D, cmap='gray', vmin=0, vmax=255)
 
 		plt.show()
 
@@ -83,6 +104,7 @@ def discrete_fourier_transform_1D(image):
 Transformée de fourier discrète 2D
 '''
 def discrete_fourier_transform_2D(image):
+	print("---- DFT 2D ----")
 	m = len(image)
 	n = len(image[0])
 
@@ -128,6 +150,7 @@ def invert_discrete_fourier_transform_1D(image):
 Inverse de la transformée de Fourier discrète 2D
 '''
 def invert_discrete_fourier_transform_2D(image):
+	print("---- IDFT 2D ----")
 	m = len(image)
 	n = len(image[0])
 
@@ -144,6 +167,128 @@ def invert_discrete_fourier_transform_2D(image):
 		idft_img[i] = invert_discrete_fourier_transform_1D(idft_img[i])
 
 	return normalise2d(transposition(idft_img))
+
+'''
+Transformée rapide
+'''
+
+'''
+Trasnformée de fourier rapide 1D
+'''
+def fast_fourier_transform_1D(image):
+	# Taille de l'image
+	N = len(image)
+	# Image finale
+	fft_image_1D = N*[0]
+
+
+	# Si l'image ne fait que un pixel, sa fft est correpondant à ce pixel
+	if N == 1:
+		return image
+
+	# Si le nombre de pixel n'est pas une puissance de 2 on arrête le traitement
+	if not math.log2(N).is_integer() :
+		print("L'image comporte",N,"pixels qui n'est pas une puissance de 2")
+		exit()
+
+	# Pour des raisons de performance on n'effectu le calcul qu'une seule fois
+	omega = np.exp(-2j*np.pi/N)
+
+	# On coupe l'image en deux groupe, pair et impair
+	image_pair = image[::2]
+	image_impair = image[1::2]
+
+	# résultat
+	fft_image_pair = fast_fourier_transform_1D(image_pair)
+	fft_image_impair = fast_fourier_transform_1D(image_impair)
+
+	# Reconstruction de l'image finale
+	for j in range(N//2):
+		fft_image_1D[j] = fft_image_pair[j] + (omega**j)*fft_image_impair[j]
+		fft_image_1D[j+N//2] = fft_image_pair[j] - (omega**j)*fft_image_impair[j]
+
+	return fft_image_1D
+
+def fast_fourier_transform_2D(image):
+	print("---- FFT 2D ----")
+	m = len(image)
+	n = len(image[0])
+
+	fft_img = m*[n*[0]]
+
+	print("Traitement des colonnes")
+	for i in tqdm(range(m)):
+		fft_img[i] = fast_fourier_transform_1D(image[i])
+
+	fft_img = transposition(fft_img)
+
+	print("Traitement des lignes")
+	for i in tqdm(range(n)):
+		fft_img[i] = fast_fourier_transform_1D(fft_img[i])
+
+	return transposition(fft_img)
+
+'''
+Transformée de Fourier rapide inverse
+'''
+
+'''
+Transformée de Fourier rapide 1D inverse
+'''
+def invert_fast_fourier_transform_1D(image):
+	# Taille de l'image
+	N = len(image)
+	# Image finale
+	ifft_image_1D = N*[0]
+
+	# Si l'image ne fait que un pixel, sa ifft est correpondant à ce pixel
+	if N == 1:
+		return image
+
+	# Si le nombre de pixel n'est pas une puissance de 2 on arrête le traitement
+	if not math.log2(N).is_integer() :
+		print("L'image comporte",N,"pixels qui n'est pas une puissance de 2")
+		exit()
+
+	# Pour des raisons de performance on n'effectu le calcul qu'une seule fois
+	omega = np.exp(2j*np.pi/N)
+
+	# On coupe l'image en deux groupe, pair et impair
+	image_pair = image[::2]
+	image_impair = image[1::2]
+
+	# résultat
+	ifft_image_pair = invert_fast_fourier_transform_1D(image_pair)
+	ifft_image_impair = invert_fast_fourier_transform_1D(image_impair)
+
+	# Reconstruction de l'image finale
+	for j in range(N//2):
+		ifft_image_1D[j] = ifft_image_pair[j] + (omega**j)*ifft_image_impair[j]
+		ifft_image_1D[j+N//2] = ifft_image_pair[j] - (omega**j)*ifft_image_impair[j]
+
+	return ifft_image_1D
+
+'''
+Transformée de Fourier rapide 2D inverse
+'''
+def invert_fast_fourier_transform_2D(image):
+	print("---- IFFT 2D ----")
+	m = len(image)
+	n = len(image[0])
+
+	ifft_image = m*[n*[0]]
+
+	print("Traitement de colonnes")
+	for i in tqdm(range(m)):
+		ifft_image[i] = invert_fast_fourier_transform_1D(image[i])
+
+	ifft_image = transposition(ifft_image)
+
+	print("Traitement des lignes")
+	for i in tqdm(range(n)):
+		ifft_image[i] = invert_fast_fourier_transform_1D(ifft_image[i])
+
+	return normalise2d(transposition(ifft_image))
 
 if __name__=="__main__":
 	main()
